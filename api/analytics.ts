94963156f510a09d8d11128522b6614b579bde54
e.gluhotorenko <egluhotorenko.external@epo.org>
2024-08-19 17:51:50 +0200

    add node typings

--- api/analytics.ts
+++ api/analytics.ts
@@ -1,5 +1,5 @@
 export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}.`);
+  return new Response(`Hello from ${process.env.VERCEL_REGION}..`);
 }
 export const config = {
   runtime: 'edge',
