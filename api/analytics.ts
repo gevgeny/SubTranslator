e229cb8cb3c747a074b9b984de3170237848619a
e.gluhotorenko <egluhotorenko.external@epo.org>
2024-08-19 21:44:57 +0200

    update analytics

--- api/analytics.ts
+++ api/analytics.ts
@@ -1,6 +1,48 @@
-export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}..`);
+export async function POST(request: Request) {
+  const params = await request.json();
+  console.log('params', params);
+  try {
+    const response = await fetch('https://queue.simpleanalyticscdn.com/events', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...params,
+        hostname: 'e.gluhotorenko.com',
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to send event. Status: ${response.status}, text: ${await response.text()}`,
+      );
+    }
+    return response;
+  } catch (error) {
+    return new Response(error.message, { status: 500 });
+  }
 }
+
+const allowedOrigins = [
+  'chrome-extension://fbkdpdfffgmkfblnonpdhhpoaglpmcpg',
+  'http://localhost:3000',
+];
+
+export async function GET(request: Request) {
+  const origin = request.headers.get('Origin') ?? '';
+  console.log('origin', origin);
+  return new Response('', {
+    headers: {
+      'Access-Control-Allow-Origin': allowedOrigins.includes(origin)
+        ? origin
+        : allowedOrigins[0],
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'POST',
+    },
+  });
+}
+
 export const config = {
   runtime: 'edge',
 };
